0919edceaeaf940eb2048036412c8b0359d53673
pinkfloydfan <jxchen98@gmail.com>
2021-03-08 14:54:31 +0000

    changed websocket client to websocket-client

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,27 @@
 import os
 
 from flask import Flask
+from flask import render_template
 
+import websocket
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import time
+# websocket boilerplate
+
+def on_message(ws, message):
+    print(message)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    print("### closed ###")
+
+def on_open(ws):
+    print("### open ###")
 
 def create_app(test_config=None):
     # create and configure the app
@@ -25,8 +45,13 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @app.route('/')
+    def index():
+        print("trying to open websocket")
+        ws = websocket.WebSocketApp("ws://192.168.4.22:80", on_open = on_open, on_message = on_message, on_error = on_error, on_close = on_close)
+        print("websocket instance created")
+        ws.run_forever()
+        return render_template('index.html',title="henlo")
+
 
     return app
